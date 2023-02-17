ea58cc30c0f66ad39ab6316748b55da0c24fa969
ekremsalli <111192078+ekremsalli@users.noreply.github.com>
2023-02-17 22:31:16 +0300

    Delete StudentPOC directory

--- StudentPOC/src/main/java/tr/com/vbt/repository/department/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package tr.com.vbt.repository.department;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tr.com.vbt.entity.department.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-	
-}
